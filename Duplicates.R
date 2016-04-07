bf5a023c4ef149786dcbeb514b89146c27cffca7
itirsnpk <itir.sonuparlak@gmail.com>
2016-04-07 10:29:10 -0400

    Updated for new data
    Added practice for UHI
    Added both sets of duplicates to the delete file for easy checking
    Added new sets of known twins
    Removed the partial exports

--- Duplicates.R
+++ Duplicates.R
@@ -6,11 +6,19 @@ path<-"Y:/Monthly Import/Feb 2016/"
 allpayers<-read.csv(paste(path, "AllPayerHIEIDs-2016-02-03.csv", sep=""), stringsAsFactors = FALSE)
 uhi<-read.csv(paste(path, "UhiHIEIDs-2016-02-03.csv", sep=""), stringsAsFactors = FALSE)
 
+# Subsets out CAMcare data
+camcare<-subset(allpayers, allpayers$VEND_FULL_NAME=="CAMCARE HEALTH CORPORATION")
+rest<-subset(allpayers, allpayers$VEND_FULL_NAME!="CAMCARE HEALTH CORPORATION")
+
+# Merges back Horizon CAMcare data
+camcarehorizon<-subset(camcare, camcare$Source=="Horizon")
+allpayers<-rbind(rest, camcarehorizon)
+
 # Renames fields
 uhi<-reshape::rename(uhi, c(HOME_PHONE_NUM="HOME_PHONE_NUMBER"))
 uhi<-reshape::rename(uhi, c(MEMB_INSURANCE="PAYER"))
-uhi<-reshape::rename(uhi, c(ï..Reg.Patient.MRN="Reg.Patient.MRN"))
-allpayers<-reshape::rename(allpayers, c(ï..BUS_PHONE_NUMBER="BUS_PHONE_NUMBER"))
+uhi<-reshape::rename(uhi, c(Ã¯..Reg.Patient.MRN="Reg.Patient.MRN"))
+allpayers<-reshape::rename(allpayers, c(Ã¯..BUS_PHONE_NUMBER="BUS_PHONE_NUMBER"))
 
 # Adds NIC to the uhi Subscriber ID if it's not there 
 uhi$SUBSCRIBER_ID<-ifelse(grepl("NIC", uhi$SUBSCRIBER_ID), uhi$SUBSCRIBER_ID, paste("NIC", uhi$SUBSCRIBER_ID, sep=""))
@@ -26,6 +34,9 @@ uhi$X <- NULL
 # Adds capitation date field
 uhi$LastCapitationDate <- format(Sys.time(), "%m/01/%Y") 
 
+# Assigns practice to the UHI file
+uhi$PRACTICE<- "Cooper IM"
+
 # Creates fields with blank values
 uhi$BUS_PHONE_NUMBER	<- "NA"
 uhi$CURR_PCP_ADDRESS_LINE_1	<- "NA"
@@ -40,7 +51,6 @@ uhi$MEDICARE_NO	<- "NA"
 uhi$PAYER	<- "NA"
 uhi$PHONE_NUMBER	<- "NA"
 uhi$VENDOR_ID	<- "NA"
-uhi$PRACTICE	<- "NA"
 
 # Sorts columns in both files
 allpayers<-allpayers[,order(names(allpayers))]
@@ -52,8 +62,24 @@ combined<-rbind(allpayers, uhi)
 # Changes case of HIE ID to lowercase
 combined$HIEID<-tolower(combined$HIEID)
 
-# Identifies duplicate values with TRUE or FALSE
-combined$duplicate<-duplicated(combined$HIEID)
+#Sorts data by HIEID
+combined <- combined[order(combined$HIEID),] 
+
+# Identifies the first pair of duplicate values - the one not to be deleted
+combined$duplicate2<- duplicated(combined$HIEID, fromLast=TRUE)
+
+# Identifies second pair of duplicate values - the one to be deleted
+combined$duplicate<-duplicated(combined$HIEID) 
+
+# Identifies the urgency with which the duplicate should be cleared (1 is important to resolve)
+combined$dupetype <- ifelse(combined$duplicate == TRUE & combined$Source == "United", "dupe1", "dupe3")
+combined$dupetype <- ifelse(combined$duplicate == TRUE & combined$Source == "Horizon", "dupe1", combined$dupetype)
+
+# Creates a list of urgent duplicates for reference in script
+dupes<-subset(combined, combined$dupetype == "dupe1")
+
+# Marks the urgency of duplicates for the second part of the pair
+combined$dupetype <- ifelse(combined$HIEID %in% dupes$HIEID, "dupe1", combined$dupetype)
 
 # Identifies known twins
 twins<-subset(combined,
@@ -65,58 +91,58 @@ twins<-subset(combined,
               combined$SUBSCRIBER_ID==101854600 |
               combined$SUBSCRIBER_ID==106274833 |
               combined$SUBSCRIBER_ID==106274834 |
+              combined$SUBSCRIBER_ID==108034384 |
+              combined$SUBSCRIBER_ID==108034385 |
+              combined$SUBSCRIBER_ID==110119063 |
+              combined$SUBSCRIBER_ID==110119064 |
               combined$SUBSCRIBER_ID=="H71260169" |
               combined$SUBSCRIBER_ID=="H71260168" |
               combined$SUBSCRIBER_ID=="H71514576" |
-              combined$SUBSCRIBER_ID=="H71514574"
+              combined$SUBSCRIBER_ID=="H71514574" |
+              combined$SUBSCRIBER_ID=="H71570565" |
+              combined$SUBSCRIBER_ID=="H71570564" |
+              combined$SUBSCRIBER_ID=="H71598715" |
+              combined$SUBSCRIBER_ID=="H71595647" |
+              combined$SUBSCRIBER_ID=="H71579478" |
+              combined$SUBSCRIBER_ID=="H71579477" |
+              combined$SUBSCRIBER_ID=="H35022345" |
+              combined$SUBSCRIBER_ID=="H35022337" |
+              combined$SUBSCRIBER_ID=="H474184"   |
+              combined$SUBSCRIBER_ID=="H474159"
               )
 
 # Subsets values that are TRUE for duplicate and are not a twin
-duplicates<-subset(combined, combined$duplicate==TRUE & !combined$SUBSCRIBER_ID %in% twins$SUBSCRIBER_ID)
+duplicates<-subset(combined, combined$duplicate==TRUE | combined$duplicate2== TRUE & !combined$SUBSCRIBER_ID %in% twins$SUBSCRIBER_ID)
+
+# Removes high confidence duplicates from the file
+duplicates<-duplicates[!(duplicates$dupetype=="dupe3" & duplicates$duplicate2==TRUE),]
 
 # Identifies variables to export
 duplicates<-duplicates[,c("HIEID",
                           "PRACTICE",
                           "Source",
+                          "MEMB_FIRST_NAME", 
+                          "MEMB_LAST_NAME",
+                          "DOB",
+                          "SOCIAL_SEC_NO",
                           "SUBSCRIBER_ID",
-                          "VEND_FULL_NAME")]
+                          "VEND_FULL_NAME",
+                          "duplicate",
+                          "duplicate2",
+                          "dupetype")]
 
 # Subsets values that are FALSE for duplicate and are not a twin
-uniques<-subset(combined, combined$duplicate==FALSE& !combined$SUBSCRIBER_ID %in% twins$SUBSCRIBER_ID)
+uniques<-subset(combined, combined$duplicate==FALSE & !combined$SUBSCRIBER_ID %in% twins$SUBSCRIBER_ID)
 
 # Renames HIEID field
 uniques<-reshape::rename(uniques, c(HIEID="Patient ID HIE"))
 
 # Removes the field that identifies duplicates
 uniques$duplicate<-NULL
-
-# Prepare twins file for export
-twins$duplicate<-NULL
-
-# Identifies number of columns
-c<-ncol(uniques)
-
-# Identifies number of rows
-r<-nrow(uniques)
-
-# Identifies row breakpoints to create smaller files to import
-r1<-round(r/4)
-r2<-round((r/4)*2)
-r3<-round((r/4)*3)
-r4<-r
-
-# Breaks the one uniques file into 4 pieces to import smaller sizes of data into database 
-# Use only if import process freezes with the larger file
-uniques1<-uniques[1:r1, 1:30]
-uniques2<-uniques[(r1+1):r2, 1:30]
-uniques3<-uniques[(r2+1):r3, 1:30]
-uniques4<-uniques[(r3+1):r, 1:30]
+uniques$duplicate2<-NULL
+uniques$dupetype<-NULL
 
 # Exports files
 write.csv(twins, paste(Sys.Date(), "-",file="Twins-New-HIE-ID",".csv", sep=""), row.names = FALSE)
 write.csv(duplicates, paste(Sys.Date(), "-",file="HIE-Delete",".csv", sep=""), row.names = FALSE)
 write.csv(uniques, paste(Sys.Date(),"-",file="TrackVia-Import", ".csv", sep=""), row.names = FALSE)
-#write.csv(uniques1, paste(Sys.Date(),"-",file="TrackVia-Import-Part-1", ".csv", sep=""))
-#write.csv(uniques2, paste(Sys.Date(),"-",file="TrackVia-Import-Part-2", ".csv", sep=""))
-#write.csv(uniques3, paste(Sys.Date(),"-",file="TrackVia-Import-Part-3", ".csv", sep=""))
-#write.csv(uniques4, paste(Sys.Date(),"-",file="TrackVia-Import-Part-4", ".csv", sep=""))
